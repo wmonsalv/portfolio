b28f4db8369261d6a2d5ab32989ea2a413e36c08
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-08 12:36:45 -0400

    added images

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,26 @@
-import "./index.scss";
+import "/Users/william_x1/portfolio/src/components/Sidebar/indexSidebar.scss";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 
 const Sidebar = () => (
     <div className="nav-bar">
         <Link className="logo" to="/">
             <image src={"/Users/william_x1/portfolio/src/assets/images/logo-s.png"} alt="logoS"/>
+            <image className="sub-logo" src={"/Users/william_x1/portfolio/src/assets/images/logo_sub.png"} alt="logoSubtitle"/>
         </Link>
+        <nav>
+            <navLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="white"/>
+            </navLink>
+            <navLink exact="true" activeclassname="about-link" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} className="a" color="white"/>
+            </navLink>
+            <navLink exact="true" activeclassname="contact-link" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="white"/>
+            </navLink>
+        </nav>
     </div>
 )
 
