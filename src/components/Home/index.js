5a33989c3c126d8d492c754cd3c57d7d60cee9ed
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-11 19:17:41 -0400

    made more changes to interface

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,23 @@
-const Home= ()=> ("Hello")
+import {Link} from "react-router-dom";
+import "./index.scss";
 
+
+
+
+const Home= ()=> {
+    
+    return(
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, 
+                    <br/> I'm William <br/> 
+                </h1>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+
+            </div>
+        </div>
+
+    )
+
+}
 export default Home
