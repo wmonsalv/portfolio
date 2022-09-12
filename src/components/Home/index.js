bc9a9710733d04ecfa975e693deb4c88b9127f4f
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-11 20:49:03 -0400

    finally got the effect to work

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -21,11 +21,11 @@ const Home= ()=> {
             <div className="text-zone">
                 <h1>
                     <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
+                    <span className={`${letterClass} _13`}>i,</span>
                     <br/> 
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/><br/> 
+                    <span className={`${letterClass} _14`}>I</span>
+                    <span className={`${letterClass} _15`}>'m</span>
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={16}/><br/> 
                 </h1>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
 
