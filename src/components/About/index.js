20af6075950fc7876c27746c3d49656cc88d11bf
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-12 18:03:40 -0400

    cube works finally

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCss3, faHtml5, faJava, faJsSquare, faPython, faReact } from "@fortawesome/free-brands-svg-icons";
 
+//it appears cube finally works
+
 const About = () => {
 
     const [letterClass, setLetterClass] = useState("text-animate")
