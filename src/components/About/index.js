14df1065668c3394a5e4f3d81c6db1fbd02834d4
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-09-12 17:59:49 -0400

    working on cube

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,8 @@
 import "./index.scss";
 import AnimatedLetters from "../AnimatedLetters"
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCss3, faHtml5, faJava, faJsSquare, faPython, faReact } from "@fortawesome/free-brands-svg-icons";
 
 const About = () => {
 
@@ -8,7 +10,7 @@ const About = () => {
     useEffect(() => {
         setTimeout(() => {
             return setLetterClass("text-animate-hover")
-        }, 4000)
+        }, 3000)
     }, [])
 
 
@@ -18,17 +20,40 @@ const About = () => {
             <div className="text-zone">
                 <h1>
                     <AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={["A", "b", "o", "u", "t", " ", "M", "e" ]}
-                    idx={15}
+                        letterClass={letterClass}
+                        strArray={["A", "b", "o", "u", "t", " ", "M", "e"]}
+                        idx={15}
                     />
                 </h1>
                 <p>
-                My name is William Monsalve, I'm a senior at Kennesaw state University majoring in Computer Science. I'm aspiring to graduate in Spring of 2023.
+                    My name is William Monsalve, I'm a senior at Kennesaw state University majoring in Computer Science. I'm aspiring to graduate in Spring of 2023.
                 </p>
                 <p>
-                Due to my interest in improving my programming skills, I have some expertise with front end development. I'm familiar with React.js, React Native, HTML, CSS, SASS, JavaSCript, Python, Java, and C++.
+                    Due to my interest in improving my programming skills, I have some expertise with front end development. I'm familiar with React.js, React Native, HTML, CSS, SASS, JavaSCript, Python, Java, and C++.
                 </p>
+            </div>
+
+            <div className="stage-cube-cont">
+                <div className="cubespinner">
+                    <div className="face1">
+                        <FontAwesomeIcon icon={faHtml5} color="#DD0031"></FontAwesomeIcon>
+                    </div>
+                    <div className="face2">
+                        <FontAwesomeIcon icon={faJava} color="#7393B3"/>
+                    </div>
+                    <div className="face3">
+                    <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+                    </div>
+                    <div className="face4">
+                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
+                    </div>
+                    <div className="face5">
+                    <FontAwesomeIcon icon={faReact} color="#5DE4D4"/>
+                    </div>
+                    <div className="face6">
+                    <FontAwesomeIcon icon={faPython} color="white"/>
+                    </div>
+                </div>
             </div>
         </div>
 
